5e84b9b12552bec1af06201845eeb3eb20925667
takahirosir <569603224@.com>
2023-06-10 07:24:53 +0000

    add pip.sh

---
+++ pip.sh
@@ -0,0 +1,7 @@
+pip install -U git+https://github.com/huggingface/transformers.git 
+pip install -U git+https://github.com/huggingface/peft.git
+pip install -U git+https://github.com/huggingface/accelerate.git
+pip install --upgrade bitsandbytes
+# pip install diffy 这里不是diffy 应该是ditty就对了
+pip install ditty==0.4.0
+pip install scipy==1.10.1
