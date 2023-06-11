a6f71e82589626684c8d76e6a73fb81e02fd931a
takahirosir <569603224@qq.com>
2023-06-11 05:49:07 +0000

    solve an important promblem by docker-gpus

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:3.10
+RUN pip install --upgrade pip
 RUN pip install -U git+https://github.com/huggingface/transformers.git 
 RUN pip install -U git+https://github.com/huggingface/peft.git
 RUN pip install -U git+https://github.com/huggingface/accelerate.git
--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 sudo docker stop $(sudo docker ps -a -q)  #stop停止所有容器
 sudo docker rm $(sudo docker ps -a -q)  #remove删除所有容器
 # sudo docker run -it --rm --name rwkv rwkv:v0.1 bash
-sudo docker run  --gpus=all --name rwkv -it -v $PWD:/rwkv rwkv:v0.1 bash
+sudo docker run  --gpus all --name rwkv -it -v $PWD:/rwkv rwkv:v0.1 bash
+# sudo docker run -it --gpus all rwkv:v0.1 --name rwkv -v $PWD:/rwkv  bash
 # 这里的$PWD是当前目录，也就是rwkv-raven-qlora-4bit-instruct目录
 # $PWD:/rwkv表示将当前目录挂载到容器的/rwkv目录下 相当于创建了一个共享目录
